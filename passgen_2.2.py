3d91bf02a60c83e2a8cae72df6b0b42cb286e8f2
Name <email@example.ru>
2022-09-05 15:46:13 +0300

    Update passgen_2.2.py

--- passgen_2.2.py
+++ passgen_2.2.py
@@ -1,37 +1,60 @@
 import random
+
 print("Этот код генерирует случайный пароль из стандартного набора переменных")
-length_input = int(input("Введите минимальную желаемую длину пароля: "))
-while length_input > 30:
-    length_input = int(input("""Вы ввели слишком большое число (или слишком маленькое число). Мы не сможем сгенерировать такой пароль :(
-Попробуйте ввести его снова: """))
-def password():
+run = 1
+
+def lengh_check(length_input):
+    try:
+        int(length_input)
+    except ValueError:
+        print("Вы ввели букву или слово. Мы не можем сгенерировать пароль из этих значений. Пожалуйста, введите число от 12 до 30: ")
+        return False
+    if int(length_input) < 12:
+        print("""Вы ввели слишком маленькое число. Мы не сможем сгенерировать такой пароль :(
+Попробуйте ввести его снова: """)
+        return False
+    elif  int(length_input) > 31:
+        print("""Вы ввели слишком большое число. Мы не сможем сгенерировать такой пароль :(
+        Попробуйте ввести его снова: """)
+        return False
+    else:
+        return True
+
+def password(length_input):
     passord_key = []
 
-    for number in range(random.randint(2, 6)):
+    for number in range(random.randint(2, 7)):
         number = random.randint(0, 9)
         passord_key.append(number)
 
-    for symbol in range(random.randint(3, 8)):
+    for symbol in range(random.randint(3, 9)):
         letter = 'abcdefghijklmnopqrstuvwxyz'
         symbol = random.choice(letter)
         passord_key.append(symbol)
 
-    for symbol_h in range(random.randint(3, 8)):
+    for symbol_h in range(random.randint(3, 9)):
         letter_h = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         symbol = random.choice(letter_h)
         passord_key.append(symbol)
 
-    for special in range(random.randint(3, 8)):
+    for special in range(random.randint(3, 9)):
         symbol_list = '!@#$%^&*()_+{}:"|>?<~╥↕╕'
         special = random.choice(symbol_list)
         passord_key.append(special)
     random.shuffle(passord_key)
     real_password = "".join(map(str, passord_key))
+
     if len(real_password) == length_input:
+        print(f"""Ваш новый пароль:
+{real_password}""")
+        print(f"Он состоит из {len(real_password)} символов.")
         return real_password
     else:
-        password()
+        password(length_input)
 
 
-print(password())
-
+while run == 1:
+    length_input = input("Введите минимальную желаемую длину пароля. Эта программа может сгенерировать пароль от 12 до 30 символов: ")
+    if lengh_check(length_input):
+        password(int(length_input))
+        run -= 1
